f0f3f019fcd137f5969306187a3c33dbf516231e
PaoloSc <paolo95ita@gmail.com>
2026-03-08 18:31:19 +0100

    Day Calendar

---
+++ script/days.js
@@ -0,0 +1,19 @@
+let day=+localStorage.day||1, d=dayNumber
+d.textContent=day
+
+function nextDay(){
+    day++
+    localStorage.day=day
+
+    d.classList.remove("flip")
+    void d.offsetWidth
+    d.classList.add("flip")
+
+    d.textContent=day
+}
+
+function resetRun() {
+    localStorage.removeItem("day")
+    d.textContent=1
+    day=1
+}
